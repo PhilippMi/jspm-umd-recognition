d332dc1d58c7356a63f5536b3f9404a5d420820e
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2016-02-01 11:13:47 +0100

    test to load UMD module with CSP

---
+++ config.js
@@ -0,0 +1,12 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: false,
+  paths: {
+    "github:*": "jspm_packages/github/*"
+  },
+
+  map: {
+    "moment": "github:moment/moment@2.11.1"
+  }
+});
